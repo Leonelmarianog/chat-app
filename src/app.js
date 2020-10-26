3bc414ba872d84d6e366f2b4231e58c7e46a9236
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-10-26 15:34:11 -0300

    Actualiza App.js al mismo estado que master

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log(`${socket.id} realizo una conexion!`);
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('chat message', (msg) => {
+    console.log('mensaje: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
